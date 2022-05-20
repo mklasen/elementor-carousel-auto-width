b6026586d51e09ac91bd702c7c6403a0917c151e
Marinus Klasen <marinus@mklasen.nl>
2022-05-20 13:34:17 +0200

    Set plugin headers

--- elementor-carousel-auto-width.php
+++ elementor-carousel-auto-width.php
@@ -1,18 +1,17 @@
 <?php
 /**
  * Plugin Name:       Elementor Carousel Auto Width
- * Plugin URI:        https://mklasen.com/
- * Description:       Handle the basics with this plugin.
- * Version:           1.10.3
+ * Plugin URI:        https://mklasen.com/plugins/elementor-carousel-auto-width/
+ * Description:       Add the auto-width class to your Elementor Image Carousels to activate this plugin
+ * Version:           1.0
  * Requires at least: 5.2
  * Requires PHP:      7.2
- * Author:            John Smith
- * Author URI:        https://author.example.com/
+ * Author:            Marinus Klasen
+ * Author URI:        https://mklasen.com
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Update URI:        https://example.com/my-plugin/
- * Text Domain:       my-basics-plugin
- * Domain Path:       /languages
+ * Update URI:        https://mklasen.com/plugins/elementor-carousel-auto-width/
+ * Text Domain:       elementor-carousel-auto-width
  */
 
 
