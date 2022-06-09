e0823a5d4f68251a28b5d2af4b185b8f8d4342fa
Marinus Klasen <marinus@mklasen.nl>
2022-06-09 13:29:41 +0200

    Change jquery load event

--- js/customize-carousels.js
+++ js/customize-carousels.js
@@ -1,14 +1,14 @@
-jQuery(window).load(function () {
+jQuery(window).on("load", function () {
 	let carousels = document.querySelectorAll(".elementor-widget-image-carousel.auto-width");
 	[...carousels].forEach(function (elem) {
 		let container = elem.querySelector(".elementor-image-carousel-wrapper");
 
-        if (container !== null) {
-    		container.swiper.params.slidesPerView = "auto";
-    		container.swiper.params.centeredSlides = true;
-    		container.swiper.params.freeMode = true;
-    		container.swiper.params.loop = true;
-    		container.swiper.update();
-        }
+		if (container !== null) {
+			container.swiper.params.slidesPerView = "auto";
+			container.swiper.params.centeredSlides = true;
+			container.swiper.params.freeMode = true;
+			container.swiper.params.loop = true;
+			container.swiper.update();
+		}
 	});
 });
